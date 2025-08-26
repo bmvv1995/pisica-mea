dfea2466ed51eb871e66c6e6b5b873e558af985b
bmvv1995 <bmvv1995@gmail.com>
2025-08-26 18:12:13 +0000

    Add TypeScript support and rename app.jsx to app.tsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import CatDressUpApp from './app.jsx'
+import CatDressUpApp from './app.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
