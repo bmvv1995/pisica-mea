25bbe8365350961cfd798536d22882e5a474ea5e
bmvv1995 <bmvv1995@gmail.com>
2025-08-26 18:01:34 +0000

    Fix: case sensitivity issue in import path

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import CatDressUpApp from './App.jsx'
+import CatDressUpApp from './app.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
